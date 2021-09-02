91827549793b13ee47d5b01cdc9edbd214220a12
Aryaputra Athallah <aryaputra.athallah@ui.ac.id>
2021-09-02 13:13:08 +0700

    Updates

---
+++ precache-manifest.5671b749c6af1dabb0b1138101c28c7a.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b04052ee84d2ce99b7e2febf6a7051ed",
+    "url": "/keeper-app-react/index.html"
+  },
+  {
+    "revision": "c14b0f98803fd44870ef",
+    "url": "/keeper-app-react/static/js/2.83bd2e9e.chunk.js"
+  },
+  {
+    "revision": "71e44ed8d680b15c0722",
+    "url": "/keeper-app-react/static/js/main.929a733d.chunk.js"
+  },
+  {
+    "revision": "925a570a761bd77c288c",
+    "url": "/keeper-app-react/static/js/runtime-main.874ed7da.js"
+  }
+]);
